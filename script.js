ce7236afb6858ef946cd477f21056e798b95f621
Sare <83638067+hollowlhjo@users.noreply.github.com>
2023-03-26 12:56:06 +0300

    commit

---
+++ script.js
@@ -0,0 +1,51 @@
+//Просто масив
+// let array = ["Hollow Knight", 'CS GO', 'Metro 2033']
+
+// for(i = 0; i < array.length; i++){
+//     console.log(array[i])
+// };
+
+// let array = []
+
+// for(i = 0; i < 4; i++){
+//     array.push(i)
+// }
+// for(j = 0; j < 4; j++){
+//     console.log(array.pop())
+// }
+
+// let array = []
+
+// for(i = 0; i < 4; i++){
+//     array.unshift(i)
+// }
+
+// for(j = 0; j < 4; j++){
+//     console.log(array.shift())
+// }
+
+// let array = []
+
+// for(i = 0; i < 20; i++){
+//    array.push(Math.floor(Math.random() * 100))
+// }
+
+// let max = array[0];
+// let min = array[0];
+// let sum = 0
+// let avg = 0
+
+// for(i = 0; i < array.length; i++){
+//  if (array[i] > max){
+//     max = array[i]
+//  }
+//  if (array[i] < min){
+//     min = array[i]
+//  }
+//  sum+=array[i]
+// }
+// avg = sum / array.length;
+
+// console.log("Max: " + max)
+// console.log("Min: " + min)
+// console.log("Average: " + avg)
